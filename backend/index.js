5edbe0ee9978aeaad2173ea3d2ed562edeace963
sjitendrakumar647 <sjitendrakumar647@gmail.com>
2025-05-22 00:01:44 +0530

    Connected to database

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,19 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 const app = express()
 
 dotenv.config();
 const PORT = process.env.PORT || 5000; 
+const URI = process.env.MONGODB_URI;
+
+try{
+    mongoose.connect(URI)
+    console.log('MongoDB connected...');
+} catch(err) {
+    console.error(err.message)
+    process.exit(1)
+}
 
 app.get('/', (req, res) => {
   res.send('Welcome to Chattrix chat application!')
